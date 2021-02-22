f2a586871e3fa9b912fa11a7a24153db1086a199
Joseph Dehoney <josephdehoney@gmail.com>
2021-02-21 17:34:06 -0800

    Created basic infrastructure for the games app and routes, taking the examples from hello app.

--- gamesApp/games/views.py
+++ gamesApp/games/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+
+# Create your views here.
+def index(request):
+    return render(request, "games/index.html")
